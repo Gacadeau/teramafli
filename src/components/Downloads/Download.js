c820d68ec2704214ec73490a37a9c1386f085848
Gacadeau <gashimwea@gmail.com>
2024-01-20 16:22:00 +0200

    first commit

--- src/components/Downloads/Download.js
+++ src/components/Downloads/Download.js
@@ -17,10 +17,9 @@ const CacheViewer = () => {
   }, [])
 
   return (
-    <div className='container mx-auto py-8'>
-      <h1 className='text-3xl font-bold mb-8'>Vidéos en cache</h1>
-
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8'>
+   <div className="Uploads flex flex-col w-full h-full bg-white rounded-3xl">
+        <h1 className='text-3xl font-bold mb-8'>Vidéos en cache</h1>
+      <div className="uploadsContainer w-full h-full pt-6 overflow-y-auto">
         {cachedVideos.map((videoUrl, index) => (
           <div key={index} className='bg-white p-4 rounded-lg shadow-md'>
             {/* Utilisez votre lecteur vidéo ou une image avec le chemin spécifié */}
