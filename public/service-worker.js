73a6b0da2c363c931292cc464c95f28d21031c08
Gacadeau <gashimwea@gmail.com>
2024-01-21 19:24:29 +0200

    first commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,56 +1,11 @@
-const CACHE_NAME = 'my-site-cache-v1';
-const urlsToCache = ['/history', '/Watch', '/downloads'];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
+const CACHE_NAME = 'mon-site-cache-v1';
+ const urlsToCache = ['/history', '/Watch','/downloads']; 
+self.addEventListener('install', event => { 
+  event.waitUntil( caches.open(CACHE_NAME).then(
+    cache => cache.addAll(urlsToCache)) ); 
+  }); 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // Clone the request to make sure it can be used by both the cache and fetch
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest)
-          .then(response => {
-            // Check if we received a valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // Clone the response to store it in the cache
-            const responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(cache => {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          });
-      })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+   event.respondWith( caches.match(event.request).then(response => {
+     // Accès au cache - réponse de retour if (réponse) { return réponse; } 
+     return fetch( event.request); }) ); 
+    }); 
