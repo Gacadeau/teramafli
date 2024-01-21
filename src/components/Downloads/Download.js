e0ac8d89a3d31e9434835c6b7000f3bee4a58d9a
Gacadeau <gashimwea@gmail.com>
2024-01-21 13:04:14 +0200

    first commit

--- src/components/Downloads/Download.js
+++ src/components/Downloads/Download.js
@@ -9,10 +9,9 @@ const CacheViewer = () => {
       const cache = await caches.open('video-cache')
       const cachedRequests = await cache.keys()
 
-      const videoUrls = cachedRequests.map(request => {
-        request.url;
-        console.log('request:',request);
-      })
+      const videoUrls = cachedRequests.map(request => request.url)
+      const videoElement = cachedRequests.map(request => request);
+      console.log('videoelem:',videoElement);
       setCachedVideos(videoUrls)
     }
 
