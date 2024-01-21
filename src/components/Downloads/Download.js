95dd88fcb42fc404b6dabb21125f2774fd80cdfe
Gacadeau <gashimwea@gmail.com>
2024-01-21 13:34:41 +0200

    first commit

--- src/components/Downloads/Download.js
+++ src/components/Downloads/Download.js
@@ -13,17 +13,13 @@ const CacheViewer = () => {
       const videoDataArray = cachedRequests.map(async (request) => {
         const videoUrl = request.url;
 
-        // Ajouter d'autres informations à récupérer depuis la vidéo en cache
-        const videoBlob = await cache.match(request).then((res) => res.blob());
-        const videoElement = document.createElement('video');
-        videoElement.src = window.URL.createObjectURL(videoBlob);
-
+        // Ne créez pas un nouvel élément vidéo ici, utilisez les informations existantes
         return {
           videoUrl,
           // Ajoutez d'autres propriétés que vous avez définies dans Describe.js
-          videoId: videoElement.id,
-          videoUniid: videoElement.uniid,
-          videoTitle: videoElement.title,
+          videoId: request.videoId, // Utilisez les informations existantes
+          videoUniid: request.videoUniid, // Utilisez les informations existantes
+          videoTitle: request.videoTitle, // Utilisez les informations existantes
         };
       });
 
@@ -39,18 +35,17 @@ const CacheViewer = () => {
     <div className="Uploads flex flex-col w-full h-full bg-white rounded-3xl">
       <h1 className='text-3xl font-bold mb-8'>Vidéos en cache</h1>
       <div className="uploadsContainer w-full h-full pt-6 overflow-y-auto">
-      {cachedVideos.map((videoData, index) => (
-        <div key={index} className='bg-white p-4 rounded-lg shadow-md'>
-          <video controls className='w-full h-48 object-cover mb-4'>
-            <source src={videoData.videoUrl} type='video/mp4' />
-            Votre navigateur ne prend pas en charge la lecture de la vidéo.
-          </video>
-          <p className='text-lg font-semibold mb-2'>Title: {videoData.videoTitle}</p>
-          {/* Ajoutez d'autres informations si nécessaire */}
-          <pre>{JSON.stringify(videoData, null, 2)}</pre>
-        </div>
-      ))}
-
+        {cachedVideos.map((videoData, index) => (
+          <div key={index} className='bg-white p-4 rounded-lg shadow-md'>
+            <video controls className='w-full h-48 object-cover mb-4'>
+              <source src={videoData.videoUrl} type='video/mp4' />
+              Votre navigateur ne prend pas en charge la lecture de la vidéo.
+            </video>
+            <p className='text-lg font-semibold mb-2'>Title: {videoData.videoTitle}</p>
+            {/* Ajoutez d'autres informations si nécessaire */}
+            <pre>{JSON.stringify(videoData, null, 2)}</pre>
+          </div>
+        ))}
       </div>
     </div>
   );
