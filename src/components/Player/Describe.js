1bfa6d4ba797a35836ff2c943ad7364c21e83024
Gacadeau <gashimwea@gmail.com>
2024-01-21 20:00:08 +0200

    first commit

--- src/components/Player/Describe.js
+++ src/components/Player/Describe.js
@@ -148,8 +148,8 @@ function Describe({ video }) {
 
   const video_Url = `https://teramafli.vercel.app/Videos/${video.Video}`;
 
-  const handleDownload = async ()=> {    
-    console.log('video:',video);
+  const handleDownload = async (videopass)=> {    
+    console.log('video:',videopass);
     setDownloading(true);
     try {
       const cache = await caches.open('video-cache')
@@ -264,7 +264,7 @@ function Describe({ video }) {
                     </svg>
                   </div>
               ) : (
-                    <div title='Download' onClick={handleDownload} className="telecharger p-3 h-[45px] text-white bg-blue-500 hover:bg-blue-700 rounded-[50%] lg:text-base text-[13px] duration-300 " >
+                    <div title='Download' onClick={() => handleDownload(video.ID, 1, liked)} className="telecharger p-3 h-[45px] text-white bg-blue-500 hover:bg-blue-700 rounded-[50%] lg:text-base text-[13px] duration-300 " >
                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 cursor-pointer">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
                       </svg>
